0cfec183966038cc04b01641f42a2d94e0f0b327
Bankk <106103625+BankkRoll@users.noreply.github.com>
2023-11-03 20:27:18 -0400

    bardsski

--- src/commands/ai/bard.js
+++ src/commands/ai/bard.js
@@ -40,18 +40,24 @@ module.exports = class BardCommand extends Command {
       let bard = new Bard(bardToken, { verbose: true });
       const response = await bard.ask(question);
 
-      const embed = createEmbed({
-        title: "Bard's Response",
-        fields: [
-          { name: "Question:", value: question, inline: false },
-          { name: "Answer:", value: response, inline: false },
-        ],
-      });
+      // Split response into chunks of 2048 characters (Discord embed description limit)
+      const chunks = [];
+      for (let i = 0; i < response.length; i += 2048) {
+        chunks.push(response.slice(i, i + 2048));
+      }
 
-      await interaction.editReply({
-        content: null,
-        embeds: [embed],
-      });
+      // Send multiple messages if the response is too long
+      for (let i = 0; i < chunks.length; i++) {
+        const embed = createEmbed({
+          title: i === 0 ? `Question: ${question}` : `Continuation...`,
+          description: chunks[i],
+        });
+
+        await interaction.followUp({
+          content: null,
+          embeds: [embed],
+        });
+      }
     } catch (error) {
       console.error(error);
       await interaction.editReply({
